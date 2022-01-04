481df573675180f6662918610643ed683ca40fc0
Alwin Nedrum <nedrum@gmail.com>
2022-01-04 13:14:53 +0100

    Added range input control

--- src/App.svelte
+++ src/App.svelte
@@ -30,8 +30,12 @@
 
 </script>
 
-<canvas bind:this={canvas} height={window.innerHeight} width={window.innerWidth}>
+<canvas bind:this={canvas} height={window.innerHeight* 0.5} width={window.innerWidth * 0.5}>
 </canvas>
+
+<div class="controls">
+	<input type="range" name="resolution" id="resolution" min="1" max="20">
+</div>
 
 <style>
 
