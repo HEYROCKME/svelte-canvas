e3b702ad6ed4b2973716e8e0aaa02fe1cccafd94
Alwin Nedrum <nedrum@gmail.com>
2022-01-04 12:43:23 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	import {onMount} from "svelte"
+
+	let canvas
+
+	onMount(()=> {
+		
+		const ctx = canvas.getContext('2d')
+		
+		ctx.fillRect(100, 100, 400, 20)
+
+	})
+
+
+
+
+
+
+</script>
+
+<canvas bind:this={canvas} height={window.innerHeight} width={window.innerWidth}>
+</canvas>
+
+<style>
+
+</style>
